1632a7d861908688da29e5645e67996140d50c59
WojtG <wojtekgg431@gmail.com>
2023-07-10 12:01:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 ('use strict');
 
-// prettier-ignore
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -20,13 +17,28 @@ class Workout {
     this.distance = distance;
     this.duration = duration;
   }
+  _setDescription() {
+    const [day, month] = new Intl.DateTimeFormat('en-GB', {
+      day: 'numeric',
+      month: 'long',
+    })
+      .format(this.date)
+      .split(' ');
+
+    const date = `${month} ${day}`;
+    this.description = `${
+      this.type[0].toUpperCase() + this.type.slice(1)
+    } on ${date}`;
+  }
 }
 
 class Running extends Workout {
+  type = 'running';
   constructor(coords, distance, duration, cadence) {
     super(coords, distance, duration);
     this.cadence = cadence;
     this.calcPace(); //dzieki temu ze jest w construcotrzre to sie wywola odrazu po utworzeniu obiektu i doda nam property this.pace
+    this._setDescription();
   }
 
   calcPace() {
@@ -36,10 +48,12 @@ class Running extends Workout {
 }
 
 class Cycling extends Workout {
+  type = 'cycling';
   constructor(coords, distance, duration, elevationGain) {
     super(coords, distance, duration);
     this.elevationGain = elevationGain;
     this.calcSpeed();
+    this._setDescription();
   }
 
   calcSpeed() {
@@ -49,9 +63,11 @@ class Cycling extends Workout {
 }
 
 class App {
+  #isClicked = false;
   #map;
   #mapEvent;
   #marker;
+  #workouts = [];
   constructor() {
     this.#getPosition(); //wywolujemy tutej get poistion wtedy odrazu jak stworzymy obiekt to sie wywola ta metoda, a obiekt tworzymy na zewnatrz klas wiec wywola sie odrazu jak zaladuje sie strona
 
@@ -95,7 +111,23 @@ class App {
     this.#mapEvent = mapE;
     form.classList.remove('hidden'); //wyswoetlamy naszą form
     inputDistance.focus();
-    this.#displayMarker();
+    if (!this.#isClicked) {
+      this.#displayMarker();
+      this.#isClicked = true;
+    }
+  }
+
+  #hideForm() {
+    form.style.display = 'none'; //to prevent new workouts sliding up
+    form.classList.add('hidden');
+    setTimeout(() => (form.style.display = 'grid'), 1000); //after one second display of a form will change back to grid
+
+    // prettier-ignore
+    inputDuration.value =
+    inputDistance.value =
+    inputCadence.value =
+    inputElevation.value =
+    '';
   }
 
   #toggleElevationField() {
@@ -103,39 +135,125 @@ class App {
     inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   } // zmieniamy klase elementom na podstawie tego ze zajdzie zmiana opcjiw elemenecie select. To dziala tutaj dobrze bo sa tylko dwie opcje
 
-  #displayMarker() {
-    const { lat, lng } = this.#mapEvent.latlng; //wycigamy z naszego eventu wspolrzedne x i y miejsca w ktore klikenlismy na mapie
-
-    this.#marker = L.marker([lat, lng], { riseOnHover: true });
-
-    this.#marker.addTo(this.#map).bindPopup(
-      L.popup({
-        maxWidth: 250,
-        minWidth: 100,
-        autoClose: false,
-        closeOnClick: false,
-        className: 'running-popup',
-      }) //dodajemy wspolrzedne klikeniacia w arr i  wyswietlamy na tych wspolrzednych  nasz znacznik,. mozmey dodac tez opcje znacnzika
-    );
-  }
-
-  #displayPopup() {
-    this.#marker
-      .setPopupContent('Workout') //okreslamy tekst naszego popupa
-      .openPopup(); // dodaejmy obiekt z opcjami popup widnow, wszytsko opisae jest w dokumentacji leaflet wiec tam szukaj wszytskiego
-  }
-
   #newWorkout(e) {
+    const validInputs = (...inputs) =>
+      inputs.every(input => Number.isFinite(input));
+
+    const allPositive = (...inputs) => inputs.every(input => input > 0);
     e.preventDefault();
 
-    //Clear input fields
-    inputDuration.value =
-      inputDistance.value =
-      inputCadence.value =
-      inputElevation.value =
-        '';
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    const { lat, lng } = this.#mapEvent.latlng;
+    let workout;
 
-    this.#displayPopup();
+    if (type === 'running') {
+      const cadence = +inputCadence.value;
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        //sprawdzenie robimyh w srodku dlatego ze moze byc wysiwtelone tylko cadence lub elevation wiec definujemy je tylko w przypadku jak jest poprawny type i tylko wtedy mozemy sprawdzic czyt jest numerem czy nie
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Running([lat, lng], distance, duration, cadence);
+    }
+
+    if (type === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs have to be positive numbers!');
+
+      workout = new Cycling([lat, lng], distance, duration, elevation);
+    }
+
+    this.#workouts.push(workout);
+
+    this.#renderWorkout(workout);
+
+    this.#hideForm();
+
+    this.#displayPopup(workout);
+
+    this.#isClicked = false;
+  }
+
+  #displayMarker() {
+    const { lat, lng } = this.#mapEvent.latlng; //wycigamy z naszego eventu wspolrzedne x i y miejsca w ktore klikenlismy na mapie
+    this.#marker = L.marker([lat, lng], { riseOnHover: true });
+    //dodajemy wspolrzedne klikeniacia w arr i  wyswietlamy na tych wspolrzednych  nasz znacznik,. mozmey dodac tez opcje znacnzika
+    this.#marker.addTo(this.#map);
+  }
+
+  #displayPopup(workout) {
+    this.#marker
+      .bindPopup(
+        L.popup({
+          maxWidth: 250,
+          minWidth: 100,
+          autoClose: false,
+          closeOnClick: false,
+          className: `${workout.type}-popup`,
+        })
+      ) // dodaejmy obiekt z opcjami popup widnow, wszytsko opisae jest w dokumentacji leaflet wiec tam szukaj wszytskiego
+      .setPopupContent(
+        `${workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'} ${workout.description}`
+      ) //okreslamy tekst naszego popupa
+      .openPopup();
+  }
+
+  #renderWorkout(workout) {
+    let html = `
+    <li class="workout workout--${workout.type}" data-id="${workout.id}">
+      <h2 class="workout__title">${workout.description}</h2>
+      <div class="workout__details">
+        <span class="workout__icon">${
+          workout.type === 'running' ? '🏃‍♂️' : '🚴‍♀️'
+        }</span>
+        <span class="workout__value">${workout.distance}</span>
+        <span class="workout__unit">km</span>
+      </div>
+      <div class="workout__details">
+      <span class="workout__icon">⏱</span>
+      <span class="workout__value">${workout.duration}</span>
+      <span class="workout__unit">min</span>
+      </div>`;
+
+    if (workout.type === 'running') {
+      html += `
+      <div class="workout__details">
+        <span class="workout__icon">⚡️</span>
+        <span class="workout__value">${workout.pace.toFixed(1)}</span>
+        <span class="workout__unit">min/km</span>
+      </div>
+      <div class="workout__details">
+        <span class="workout__icon">🦶🏼</span>
+        <span class="workout__value">${workout.cadence}</span>
+        <span class="workout__unit">spm</span>
+      </div>
+    </li>`;
+    }
+
+    if (workout.type === 'cycling') {
+      html += `
+      <div class="workout__details">
+        <span class="workout__icon">⚡️</span>
+        <span class="workout__value">${workout.speed.toFixed(1)}</span>
+        <span class="workout__unit">km/h</span>
+      </div>
+      <div class="workout__details">
+          <span class="workout__icon">⛰️</span>
+          <span class="workout__value">${workout.elevationGain}</span>
+          <span class="workout__unit">m</span>
+      </div>
+    </li>
+        `;
+    }
+    form.insertAdjacentHTML('afterend', html);
   }
 }
 
